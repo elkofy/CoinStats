5dbf6470ab9d3c9d8ceb9a347fa609303bdde1dc
Baptif <baptdelhomme@gmail.com>
2021-11-21 16:12:09 +0100

    modification appel et style ajax

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
+    let limit = 5;
 
     // Appel Ajax
     function ajax() {
         $.ajax({
-            url: "https://api.coinstats.app/public/v1/coins?skip=0&limit=10&currency=EUR",
+            url: "https://api.coinstats.app/public/v1/coins?skip=0&limit="+limit+"&currency=EUR",
             method: "GET",
             dataType: "json",
         }).then(function (response) {
             let data = response.coins;
+            let size = 50;
 
             for (let crypto of data) {
-                $("#listeCrypto").append('<tr> <td>' + crypto.rank + '</td>  <td> <img src='+ crypto.icon +'> </td> <td>' + crypto.name + '</td> <td>' + crypto.price + ' €</td></tr>');
-                console.log(crypto.name);
+                let price = (crypto.price).toFixed(3);
+
+                $("#listeCrypto").append('<tr> <td>' + crypto.rank + '</td>  <td> <img src='+ crypto.icon +' width ='+size+' height ='+
+                size+'> </td> <td>' + crypto.name + '</td> <td>' + price + ' €</td> <td>' + crypto.priceChange1d + '</td> <td>' + crypto.symbol + '</td> </tr>');
             }
         });
     }
